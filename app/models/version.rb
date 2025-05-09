8d30b3109a75c23ad1f5c78a4d1c7e0a523939fe
Klaus Zanders <k.zanders@openproject.com>
2025-05-09 12:20:37 +0200

    fix total spent hours query for version

--- app/models/version.rb
+++ app/models/version.rb
@@ -81,8 +81,8 @@ class Version < ApplicationRecord
   def spent_hours
     @spent_hours ||= TimeEntry
       .not_ongoing
-      .includes(:work_package)
-      .where(work_packages: { version_id: id })
+      .joins("INNER JOIN work_packages ON entity_type = 'WorkPackage' AND work_packages.id = entity_id")
+      .where(work_packages: { version_id: id }, entity_type: "WorkPackage")
       .sum(:hours)
       .to_f
   end
